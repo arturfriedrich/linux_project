c95413907723fac3ba845489ad308fb29bba62cf
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-05-25 14:48:51 +0200

    get windspeed and clouds

--- main.sh
+++ main.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # set OpenWeatherMap API key
-API_KEY="627560b1a8d58a1751bb8b9e1ba1cc30"
+apikey=$(cat apikey.txt)
+API_KEY=$apikey
 
 # prompt user to enter a location
-# read -p "Enter a location: " location
+read -p "Enter a location: " location
 
 # call OpenWeatherMap API and retrieve weather data
-weather_data=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=Sopron&appid=${API_KEY}")
+weather_data=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}")
 
 # extract relevant weather information from JSON response using grep and cut
 temperature=$(echo "$weather_data" | grep -o '"temp":[^,]*' | cut -d ":" -f 2)
 description=$(echo "$weather_data" | grep -o '"description":"[^"]*"' | cut -d ":" -f 2 | tr -d '"')
-humidity=$(echo "$weather_data" | grep -o '"humidity":[^,]*' | cut -d ":" -f 2)
+humidity=$(echo "$weather_data" | grep -o '"humidity":[^,]*' | cut -d ":" -f 2 | tr -d '}')
+wind_speed=$(echo "$weather_data" | grep -o '"speed":[^,]*' | cut -d ":" -f 2)
+clouds=$(echo "$weather_data" | grep -o '"all":[^,]*' | cut -d ":" -f 2 | tr -d '}')
 
 # convert temperature from Kelvin to Celsius
 temperature=$(echo "scale=2; ${temperature} - 273.15" | bc)
@@ -25,5 +28,8 @@ else
   echo "Temperature in Sopron: ${temperature} °C"
 fi
 
+echo " "
 echo "Description: ${description}"
 echo "Humidity: ${humidity}%"
+echo "Wind speed: ${wind_speed}"
+echo "Clouds: ${clouds}%"
