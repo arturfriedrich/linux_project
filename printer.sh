469b03bf4c7072de88827b2509e23070b5b8e4e0
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-05-25 17:05:58 +0200

    added colors

--- printer.sh
+++ printer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 print_information() {
+
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    MAGENTA='\033[0;35m'
+    CYAN='\033[0;36m'
+    NC='\033[0m' # No color
+
     local desc=$1
     local temp=$2
     local hum=$3
@@ -12,27 +21,27 @@ print_information() {
     # Drawing ASCII art based on the desciption
     case $desc in
         "clear sky")
-            echo "   \   /     $desc"
-            echo "    .-.      $temp °C"
-            echo " ― (   ) ―   ↘ $wind km/h"
-            echo "    '-'      $hum %"
-            echo "   /   \     $clouds %"
+            echo "${YELLOW}   \   /     ${NC} $desc"
+            echo "${YELLOW}    .-.      ${NC} $temp ${GREEN}°C${NC}"
+            echo "${YELLOW} ― (   ) ―   ${NC} ↘ $wind ${GREEN}km/h${NC}"
+            echo "${YELLOW}    '-'      ${NC} $hum ${GREEN}%${NC}"
+            echo "${YELLOW}   /   \     ${NC} $clouds ${GREEN}%${NC}"
         ;;
 
         "broken clouds")
             echo "               $desc"
-            echo "      .--.     $temp °C"
-            echo "   .-(    ).   ↘ $wind km/h"
-            echo "  (___.__)__)  $hum %"
-            echo "               $clouds %"
+            echo "      .--.     $temp ${GREEN}°C${NC}"
+            echo "   .-(    ).   ↘ $wind ${GREEN}km/h${NC}"
+            echo "  (___.__)__)  $hum ${GREEN}%${NC}"
+            echo "               $clouds ${GREEN}%${NC}"
         ;;
 
-        "partly cloudy")
-            echo "     \  /       $desc"
-            echo "   _ /''.-.     $temp °C"
-            echo "     \_(   ).   ↘ $wind km/h"
-            echo "     /(___(__)  $hum %"
-            echo "                $clouds %"
+        "partly cloudy" | "few clouds")
+            echo "${YELLOW}     \  /${NC}       $desc"
+            echo "${YELLOW}   _ /''${NC}${WHITE}.-.${NC}     $temp ${GREEN}°C${NC}"
+            echo "${YELLOW}     \_${NC}${WHITE}(   ).${NC}   ↘ $wind ${GREEN}km/h${NC}"
+            echo "${YELLOW}     /${NC}${WHITE}(___(__)${NC}  $hum ${GREEN}%${NC}"
+            echo "                $clouds ${GREEN}%${NC}"
         ;;
 
         "shower rain")
