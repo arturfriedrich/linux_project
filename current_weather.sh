49d9d33652ce107deea80f55e66ecd03d800dab9
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-05-31 18:50:30 +0200

    made current weather to a separate file

---
+++ current_weather.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source printer.sh
+
+# Set OpenWeatherMap API key, write your own API key here
+apikey=$(cat apikey.txt)
+API_KEY=$apikey
+
+# Define function to retrieve weather information
+get_weather_information() {
+    local location=$1
+
+    # Call OpenWeatherMap API and retrieve weather data
+    weather_data=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}")
+
+    # Extract relevant weather information from JSON response using grep and cut
+    temperature=$(echo "$weather_data" | grep -o '"temp":[^,]*' | cut -d ":" -f 2)
+    description=$(echo "$weather_data" | grep -o '"description":"[^"]*"' | cut -d ":" -f 2 | tr -d '"')
+    humidity=$(echo "$weather_data" | grep -o '"humidity":[^,]*' | cut -d ":" -f 2 | tr -d '}')
+    wind_speed=$(echo "$weather_data" | grep -o '"speed":[^,]*' | cut -d ":" -f 2)
+    clouds=$(echo "$weather_data" | grep -o '"all":[^,]*' | cut -d ":" -f 2 | tr -d '}')
+
+    # Convert temperature from Kelvin to Celsius
+    temperature=$(echo "scale=2; ${temperature} - 273.15" | bc)
+
+    # Print weather information
+    print_information "$description" $temperature $humidity, $wind_speed $clouds
+}
