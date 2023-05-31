e970a2683b066ca34db7e18bf058054510463baa
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-05-31 18:50:57 +0200

    simplify

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source printer.sh
+source current_weather.sh
+source forecast.sh
 
 # set OpenWeatherMap API key, write your own API key here
 apikey=$(cat apikey.txt)
@@ -9,19 +11,12 @@ API_KEY=$apikey
 # prompt user to enter a location
 read -p "Enter a location: " location
 
-# call OpenWeatherMap API and retrieve weather data
-weather_data=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}")
+read -p "Do you want to display the current weather or the forecast for 5 days: (c/f) " display_mode
 
-# extract relevant weather information from JSON response using grep and cut
-temperature=$(echo "$weather_data" | grep -o '"temp":[^,]*' | cut -d ":" -f 2)
-description=$(echo "$weather_data" | grep -o '"description":"[^"]*"' | cut -d ":" -f 2 | tr -d '"')
-humidity=$(echo "$weather_data" | grep -o '"humidity":[^,]*' | cut -d ":" -f 2 | tr -d '}')
-wind_speed=$(echo "$weather_data" | grep -o '"speed":[^,]*' | cut -d ":" -f 2)
-clouds=$(echo "$weather_data" | grep -o '"all":[^,]*' | cut -d ":" -f 2 | tr -d '}')
-
-# convert temperature from Kelvin to Celsius
-temperature=$(echo "scale=2; ${temperature} - 273.15" | bc)
-
-echo "Weather information for" $location
-
-print_information "$description" $temperature $humidity, $wind_speed $clouds
+if [ "$display_mode" = "c" ]; then
+    get_weather_information "$location"
+elif [ "$display_mode" = "f" ]; then
+    get_weather_forecast "$location"
+else
+    echo "Invalid input!"
+fi
