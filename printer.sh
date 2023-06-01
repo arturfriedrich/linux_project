7e1dfbf695e516de338720501165770f3cb8cd73
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-06-01 10:57:38 +0200

    no os check is needed

--- printer.sh
+++ printer.sh
@@ -17,139 +17,71 @@ print_information() {
     local clouds=$5
     local date=$6
 
-    # Check the operating system
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS
-        case $desc in
-        "clear sky")
-            echo $date
-            echo "${YELLOW}   \   /     ${NC} $desc"
-            echo "${YELLOW}    .-.      ${NC} $temp ${GREEN}°C${NC}"
-            echo "${YELLOW} ― (   ) ―   ${NC} ↘ $wind ${GREEN}km/h${NC}"
-            echo "${YELLOW}    '-'      ${NC} $hum ${GREEN}%${NC}"
-            echo "${YELLOW}   /   \     ${NC} $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
+    case $desc in
+    "clear sky")
+        echo -n $date
+        echo ""
+        echo -e "${YELLOW}   \   /     ${NC} $desc"
+        echo -e "${YELLOW}    .-.      ${NC} $temp ${GREEN}°C${NC}"
+        echo -e "${YELLOW} ― (   ) ―   ${NC} ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "${YELLOW}    '-'      ${NC} $hum ${GREEN}%${NC}"
+        echo -e "${YELLOW}   /   \     ${NC} $clouds ${GREEN}%${NC}"
+        echo ""
+    ;;
 
-        "broken clouds")
-            echo $date
-            echo "               $desc"
-            echo "      .--.     $temp ${GREEN}°C${NC}"
-            echo "   .-(    ).   ↘ $wind ${GREEN}km/h${NC}"
-            echo "  (___.__)__)  $hum ${GREEN}%${NC}"
-            echo "               $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
+    "broken clouds")
+        echo -n $date
+        echo ""
+        echo -e "               $desc"
+        echo -e "      .--.     $temp ${GREEN}°C${NC}"
+        echo -e "   .-(    ).   ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "  (___.__)__)  $hum ${GREEN}%${NC}"
+        echo -e "               $clouds ${GREEN}%${NC}"
+        echo ""
+    ;;
 
-        "partly cloudy" | "few clouds" | "overcast clouds" | "scattered clouds")
-            echo $date
-            echo "${YELLOW}     \  /${NC}       $desc"
-            echo "${YELLOW}   _ /''${NC}${WHITE}.-.${NC}     $temp ${GREEN}°C${NC}"
-            echo "${YELLOW}     \_${NC}${WHITE}(   ).${NC}   ↘ $wind ${GREEN}km/h${NC}"
-            echo "${YELLOW}     /${NC}${WHITE}(___(__)${NC}  $hum ${GREEN}%${NC}"
-            echo "                $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
+    "partly cloudy" | "few clouds" | "overcast clouds" | "scattered clouds")
+        echo -n $date
+        echo ""
+        echo -e "${YELLOW}     \  /${NC}       $desc"
+        echo -e "${YELLOW}   _ /''${NC}${WHITE}.-.${NC}     $temp ${GREEN}°C${NC}"
+        echo -e "${YELLOW}     \_${NC}${WHITE}(   ).${NC}   ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "${YELLOW}     /${NC}${WHITE}(___(__)${NC}  $hum ${GREEN}%${NC}"
+        echo -e "                $clouds ${GREEN}%${NC}"
+        echo ""
+    ;;
 
-        "shower rain")
-            echo $date
-            echo "${YELLOW}  _'/''${NC}.-.       $desc"
-            echo "${YELLOW}   ,\_${NC}(   ).     $temp ${GREEN}°C${NC}"
-            echo "${YELLOW}    /${NC}(___(__)    ↘ $wind ${GREEN}km/h${NC}"
-            echo "${BLUE}      ' ' ' '${NC}    $hum ${GREEN}%${NC}"
-            echo "${BLUE}     ' ' ' '${NC}     $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
+    "shower rain")
+        echo -n $date
+        echo ""
+        echo -e "${YELLOW}  _'/''${NC}.-.       $desc"
+        echo -e "${YELLOW}   ,\_${NC}(   ).     $temp ${GREEN}°C${NC}"
+        echo -e "${YELLOW}    /${NC}(___(__)    ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "${BLUE}      ' ' ' '${NC}    $hum ${GREEN}%${NC}"
+        echo -e "${BLUE}     ' ' ' '${NC}     $clouds ${GREEN}%${NC}"
+        echo ""
+    ;;
 
-        "rain" | "light rain" | "moderate rain")
-            echo $date
-            echo "      .--.      $desc"
-            echo "   .-(    ).    $temp ${GREEN}°C${NC}"
-            echo "  (___.__)__)   ↘ $wind ${GREEN}km/h${NC}"
-            echo "${BLUE}  ' ' ' ' ' '${NC}   $hum ${GREEN}%${NC}"
-            echo "${BLUE}   ' ' ' ' '${NC}    $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
+    "rain" | "light rain" | "moderate rain")
+        echo -n $date
+        echo ""
+        echo -e "      .--.      $desc"
+        echo -e "   .-(    ).    $temp ${GREEN}°C${NC}"
+        echo -e "  (___.__)__)   ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "${BLUE}  ' ' ' ' ' '${NC}   $hum ${GREEN}%${NC}"
+        echo -e "${BLUE}   ' ' ' ' '${NC}    $clouds ${GREEN}%${NC}"
+        echo ""
+    ;;
 
-        *)
-            echo $date
-            echo "${YELLOW}             ${NC} $desc"
-            echo "${YELLOW}             ${NC} $temp ${GREEN}°C${NC}"
-            echo "${YELLOW}             ${NC} ↘ $wind ${GREEN}km/h${NC}"
-            echo "${YELLOW}             ${NC} $hum ${GREEN}%${NC}"
-            echo "${YELLOW}             ${NC} $clouds ${GREEN}%${NC}"
-            echo ""
-
-    esac
-    else
-        # Linux
-        case $desc in
-        "clear sky")
-            echo -n $date
-            echo ""
-            echo -e "${YELLOW}   \   /     ${NC} $desc"
-            echo -e "${YELLOW}    .-.      ${NC} $temp ${GREEN}°C${NC}"
-            echo -e "${YELLOW} ― (   ) ―   ${NC} ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "${YELLOW}    '-'      ${NC} $hum ${GREEN}%${NC}"
-            echo -e "${YELLOW}   /   \     ${NC} $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
-
-        "broken clouds")
-            echo -n $date
-            echo ""
-            echo -e "               $desc"
-            echo -e "      .--.     $temp ${GREEN}°C${NC}"
-            echo -e "   .-(    ).   ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "  (___.__)__)  $hum ${GREEN}%${NC}"
-            echo -e "               $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
-
-        "partly cloudy" | "few clouds" | "overcast clouds" | "scattered clouds")
-            echo -n $date
-            echo ""
-            echo -e "${YELLOW}     \  /${NC}       $desc"
-            echo -e "${YELLOW}   _ /''${NC}${WHITE}.-.${NC}     $temp ${GREEN}°C${NC}"
-            echo -e "${YELLOW}     \_${NC}${WHITE}(   ).${NC}   ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "${YELLOW}     /${NC}${WHITE}(___(__)${NC}  $hum ${GREEN}%${NC}"
-            echo -e "                $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
-
-        "shower rain")
-            echo -n $date
-            echo ""
-            echo -e "${YELLOW}  _'/''${NC}.-.       $desc"
-            echo -e "${YELLOW}   ,\_${NC}(   ).     $temp ${GREEN}°C${NC}"
-            echo -e "${YELLOW}    /${NC}(___(__)    ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "${BLUE}      ' ' ' '${NC}    $hum ${GREEN}%${NC}"
-            echo -e "${BLUE}     ' ' ' '${NC}     $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
-
-        "rain" | "light rain" | "moderate rain")
-            echo -n $date
-            echo ""
-            echo -e "      .--.      $desc"
-            echo -e "   .-(    ).    $temp ${GREEN}°C${NC}"
-            echo -e "  (___.__)__)   ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "${BLUE}  ' ' ' ' ' '${NC}   $hum ${GREEN}%${NC}"
-            echo -e "${BLUE}   ' ' ' ' '${NC}    $clouds ${GREEN}%${NC}"
-            echo ""
-        ;;
-
-        *)
-            echo -n $date
-            echo ""
-            echo -e "${YELLOW}             ${NC} $desc"
-            echo -e "${YELLOW}             ${NC} $temp ${GREEN}°C${NC}"
-            echo -e "${YELLOW}             ${NC} ↘ $wind ${GREEN}km/h${NC}"
-            echo -e "${YELLOW}             ${NC} $hum ${GREEN}%${NC}"
-            echo -e "${YELLOW}             ${NC} $clouds ${GREEN}%${NC}"
-            echo ""
+    *)
+        echo -n $date
+        echo ""
+        echo -e "${YELLOW}             ${NC} $desc"
+        echo -e "${YELLOW}             ${NC} $temp ${GREEN}°C${NC}"
+        echo -e "${YELLOW}             ${NC} ↘ $wind ${GREEN}km/h${NC}"
+        echo -e "${YELLOW}             ${NC} $hum ${GREEN}%${NC}"
+        echo -e "${YELLOW}             ${NC} $clouds ${GREEN}%${NC}"
+        echo ""
 
     esac
-    fi
-
 }
