bb6a462403d0a405adb686dbfd729b343142d5b7
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-06-01 09:24:35 +0200

    comments

--- main.sh
+++ main.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+# Include the necessary scripts
 source printer.sh
 source current_weather.sh
 source forecast.sh
 
-# set OpenWeatherMap API key, write your own API key here
+# Set OpenWeatherMap API key, write your own API key here
 apikey=$(cat apikey.txt)
 API_KEY=$apikey
 
-# prompt user to enter a location
+# Prompt user to enter a location
 read -p "Enter a location: " location
 
+# Prompt user to choose display mode: current weather (c) or forecast (f)
 read -p "Do you want to display the current weather or the forecast for 5 days: (c/f) " display_mode
 
+# Check the value of display_mode and take appropriate action
 if [ "$display_mode" = "c" ]; then
+    # Display current weather information
     get_weather_information "$location"
 elif [ "$display_mode" = "f" ]; then
+    # Display weather forecast for 5 days
     get_weather_forecast "$location"
 else
     echo "Invalid input!"
